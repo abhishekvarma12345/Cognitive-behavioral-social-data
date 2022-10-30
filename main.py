b370cfeb63c68569755996bd1609a2782ad5f9d0
abhishekvarma12345 <abhishekvarmad@gmail.com>
2022-10-30 07:19:19 +0100

    successfully read the data

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from feature_selection.data_mngt import read_data
+
+## follow PEP8 standards 
+# Class names must be camelcase (Ex: DataManagement)
+# function and variable names must be lowercase with words separated by underscore (Ex: read_data, file_path)
+
+if __name__ == '__main__':
+    datasets_dir = os.path.join(os.getcwd(), 'Datasets')
+    folder_name = "5. PHQ9_GAD7"
+    filename = "PHQ9_GAD7_df.csv"
+    file_path = os.path.join(datasets_dir, folder_name, filename)
+    df = read_data(file_path)
+    

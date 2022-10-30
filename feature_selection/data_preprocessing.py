52aa4883dbd9a226ea15e1cda9e602a28cd9052a
abhishekvarma12345 <abhishekvarmad@gmail.com>
2022-10-30 07:23:16 +0100

    starter files created

---
+++ feature_selection/data_preprocessing.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+def imbalance_check(data:pd.DataFrame):
+    pass
+
+def scale_data(data:pd.DataFrame)->pd.DataFrame:
+    pass
+
+
---
+++ feature_selection/models.py
@@ -0,0 +1,14 @@
+import sklearn as sk
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LassoCV
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.inspection import permutation_importance
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+from sklearn.feature_selection import mutual_info_classif
+

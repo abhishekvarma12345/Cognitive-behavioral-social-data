c4cca36030352dbf91263ed668e6b8a8d5ef38ce
abhishekvarma12345 <abhishekvarmad@gmail.com>
2022-10-31 16:38:45 +0100

    tested tree based models

--- feature_selection/models.py
+++ feature_selection/models.py
@@ -1,4 +1,5 @@
-import sklearn as sk
+from feature_selection.utils import save_plot
+
 
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -11,4 +12,47 @@ from sklearn.inspection import permutation_importance
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 from sklearn.feature_selection import mutual_info_classif
+from sklearn.feature_selection import SelectFromModel
+
+
+def dtree(X, y, action=None):
+    # define the model
+    model = DecisionTreeClassifier()
+    # fit the model
+    model.fit(X, y)
+    # get importance
+    importance = model.feature_importances_
+    # summarize feature importance
+    for i,v in enumerate(importance):
+        print('Feature: %0d, Score: %.5f' % (i,v))
+    save_plot(X.columns, importance, 'dtree.png')
+
+def rforest(X, y, action=None):
+    # define the model
+    model = RandomForestClassifier()
+    # fit the model
+    model.fit(X, y)
+    # get importance
+    importance = model.feature_importances_
+    # summarize feature importance
+    for i,v in enumerate(importance):
+        print('Feature: %0d, Score: %.5f' % (i,v))
+    save_plot(X.columns, importance, 'rforest.png')
+
+def xgboost(X, y, action=None):
+    model = XGBClassifier()
+    # fit the model
+    model.fit(X, y)
+    # get importance
+    importance = model.feature_importances_
+    # summarize feature importance
+    for i,v in enumerate(importance):
+        print('Feature: %0d, Score: %.5f' % (i,v))
+    save_plot(X.columns, importance, 'xgboost.png')
+
+
+
+    
+
+
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from feature_selection.data_mngt import read_data, split_data
-from feature_selection.data_preprocessing import imbalance_check
+from feature_selection.data_preprocessing import imbalance_check, label_encoding, scale_data
+from feature_selection.models import dtree, rforest, xgboost
 
 ## follow PEP8 standards 
 # Class names must be camelcase (Ex: DataManagement)
@@ -13,8 +14,29 @@ if __name__ == '__main__':
     file_path = os.path.join(datasets_dir, folder_name, filename)
     df = read_data(file_path)
 
+    # checking for class imbalance
     print("imbalanced classes:",imbalance_check(df))
 
+    # splitting the data into train and test sets
     X_train, X_test, y_train, y_test = split_data(df, threshold=0.8)
 
+    # scaling data using standard scaler by default(use scaler="minmax" for minmax scaling)
+    X_train_scaled, X_test_scaled = scale_data(X_train, X_test)
     
+    # encoding labels into 0 and 1
+    y_train_encoded = label_encoding(y_train)
+    y_test_encoded = label_encoding(y_test)
+    assert y_train.value_counts().loc['H'] == y_train_encoded.value_counts().loc[1]
+
+    # model training for feature selection
+    dtree(X_train_scaled, y_train_encoded)
+    print("end of decision tree".center(50,"*"))
+
+    rforest(X_train_scaled, y_train_encoded)
+    print("end of random forest".center(50,'*'))
+
+    xgboost(X_train_scaled, y_train_encoded)
+    print("end of xgboost".center(50,'*'))
+
+    
+

27e287aea0c26d34383ddf086c336d8fc14a3743
abhishekvarma12345 <abhishekvarmad@gmail.com>
2022-11-22 19:14:24 +0100

    added constants folder

---
+++ feature_selection/constants/__init__.py
@@ -0,0 +1,12 @@
+SOURCE_DIR = "feature_selection"
+DATASETS_DIR = "datasets"
+
+SPLIT_THRESHOLD = 0.8
+
+PRINT_FEATURES = False
+NUM_FEATURES_TO_SELECT = 2
+PLOTFORMAT = ".png"
+
+DECISION_TREE = "dtree"
+RANDOM_FOREST = "rforest"
+XGBOOST = "xgboost"
---
+++ feature_selection/constants/datasets.py
@@ -0,0 +1,18 @@
+DATASETS = {
+    "1. shortDT_1":"DT_df_CC.csv",
+    "1. shortDT_2": "DT_df_JI.csv",
+    "2. PRMQ": "PRMQ_df.csv" ,
+    "3. PCL": "PCL5_df.csv",
+    "4. NAQ_R": "NAQ_R_df.csv",
+    "5. PHQ9_GAD7": "PHQ9_GAD7_df.csv",
+    "6. PID5": "PID5_df.csv",
+    "7. shortPID5": "sPID-5_df.csv",
+    "8. PRFQ": "PRFQ_df.csv",
+    "9. IESR": "IESR_df.csv",
+    "10. R_NEO_PI": "faked_honest_combined.csv",
+    "11. DDDT": "RAW_DDDT.CSV",
+    "12. IADQ": "IADQ_df.csv",
+    "13. BF_1": "BF_df_CTU.csv",
+    "13. BF_2": "BF_df_OU.csv", 
+    "13. BF_3": "BF_df_V.csv"
+}
